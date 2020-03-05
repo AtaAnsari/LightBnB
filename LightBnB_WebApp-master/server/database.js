3235cacda9e856efc57eedb0680c1d836c4f50d2
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-03-05 22:24:39 +0000

    modified database queries to accomodate filtering

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -95,15 +95,57 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT *
+  const queryParams = [];
+  const whereClauses = []
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  LIMIT $1;
-  `, [limit])
-  .then(res => res.rows)
-  .catch(err => console.error('query error', err.stack));
-  
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    whereClauses.push(`city ILIKE $${queryParams.length} `);
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    whereClauses.push(`owner_id = $${queryParams.length} `);
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryParams.push(`${options.options.maximum_price_per_night}`);
+    whereClauses.push(`cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `);
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    whereClauses.push(`property_reviews.rating >= $${queryParams.length}`);
+  }
+
+  if (whereClauses.length) {
+    queryString += `WHERE ${whereClauses.join(' AND ')}`;
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams)
+  .then(res => res.rows);
 }
+
 exports.getAllProperties = getAllProperties;
 
 
